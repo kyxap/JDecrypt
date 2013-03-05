c83d628c36e36974b9290cdb075c125557871740
Roman K <rkukharuk@gmail.com>
2013-03-05 13:13:25 +0200

    fixed problem with code like: E2M3JP08891G963 version changed to 0.0.5 added debugin code (console only)

--- PS5_JDecryptor_with_AWT/src/com/kukharuk/jdecrypter/awt/Decryptions.java
+++ PS5_JDecryptor_with_AWT/src/com/kukharuk/jdecrypter/awt/Decryptions.java
@@ -1,8 +1,8 @@
 package com.kukharuk.jdecrypter.awt;
 
-//
-public class Decryptions {
 
+public class Decryptions {
+	//bidlocode just use verification input DATA is string or int
 	public boolean checkString(String string) {
         try {
             Integer.parseInt(string);
--- PS5_JDecryptor_with_AWT/src/com/kukharuk/jdecrypter/awt/JDecrypter.java
+++ PS5_JDecryptor_with_AWT/src/com/kukharuk/jdecrypter/awt/JDecrypter.java
@@ -4,14 +4,20 @@ import java.awt.event.*;  // using AWT events and listener interfaces
 
 //1NF3Z58N3805F47 - use this code for input
 
+//E2M3JP08891G963 - error with this code on 0.0.4
+
 // An AWT GUI program inherits the top-level container java.awt.Frame
 public class JDecrypter extends Frame implements ActionListener {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private Label lblInput;     // declare input Label
 	private Label lblOutput;    // declare output Label
 	private TextField tfInput;  // declare input TextField
 	private TextField tfOutput; // declare output TextField
 	private String code;       // input code
-	private String ver = "0.0.4"; 	//product version
+	private String ver = "0.0.5"; 	//product version
 	private Button btnDecr;   // declare component Button
 	private String aai = "AAAAAAAAAAAAAA9";
 	private String aia = "A9999999999AAAA";
@@ -21,6 +27,7 @@ public class JDecrypter extends Frame implements ActionListener {
 	private String aii = "AAA900000000000";
 	private String iia = "90000000000000A";
 	private String decrypt = "";
+	
 	Decryptions dec = new Decryptions();
 	
 	/** Constructor to setup the GUI */
@@ -76,10 +83,8 @@ public class JDecrypter extends Frame implements ActionListener {
 		code = tfInput.getText();
 		//TODO: rewrite this part, should be more clearly and shortly 
 		try {
-			if (dec.checkString(code.substring(5,6)) == false && dec.checkString(code.substring(12,13)) == true) {
-				decrypt = iai;
-			}
-			else if (dec.checkString(code.substring(2,3)) == true && dec.checkString(code.substring(5,6)) == true && dec.checkString(code.substring(12,13)) == true) {
+			
+			if (dec.checkString(code.substring(2,3)) == true && dec.checkString(code.substring(5,6)) == true && dec.checkString(code.substring(12,13)) == true) {
 				decrypt = iii;
 				}
 			else if (dec.checkString(code.substring(2,3)) == false && dec.checkString(code.substring(5,6)) == true && dec.checkString(code.substring(12,13)) == false) {
@@ -96,11 +101,20 @@ public class JDecrypter extends Frame implements ActionListener {
 			}
 			else if (dec.checkString(code.substring(2,3)) == true && dec.checkString(code.substring(5,6)) == true && dec.checkString(code.substring(12,13)) == false) {
 				decrypt = iia;
-				}
 			}
+			else if (dec.checkString(code.substring(5,6)) == false && dec.checkString(code.substring(12,13)) == true) {
+				decrypt = iai;
+			}
+		}
 			catch (Exception e) {
 				decrypt = "Incorrect data";
 			}
+		
+		//For debuging (3,6,13 characters)
+		//System.out.println("3th=  " + code.substring(2,3));
+		//System.out.println("6th=  " + code.substring(5,6));
+		//System.out.println("13th= " + code.substring(12,13));		
+		//System.out.println("Password: " + decrypt);
 		
 		//tfInput.setText(""); //clear input TextField
 		tfOutput.setText(decrypt); // display decrypt on the output TextField
